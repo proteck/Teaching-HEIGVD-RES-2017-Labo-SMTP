9291c99e48095bdea86327fd513dd31684f29eca
Maxime Guillod <maxime@proteck.ch>
2017-04-20 21:06:33 +0200

    Init project & gitignore

---
+++ src/teaching/heigvd/res/pkg2017/labo/smtp/Launcher.java
@@ -0,0 +1,15 @@
+package teaching.heigvd.res.pkg2017.labo.smtp;
+
+/**
+ *
+ * @author Maxime Guillod
+ */
+public class Launcher {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    }
+
+}
